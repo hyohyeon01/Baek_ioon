6f60626bf27c5f2770bbfc2bc0e9531cdfd2f4da
hyohyeon01 <162984147+hyohyeon01@users.noreply.github.com>
2024-03-26 20:16:57 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+	long long int a;
+	long long int b;
+	long long int c;
+	scanf("%lld %lld %lld", &a, &b, &c);
+
+	printf("%lld", a + b + c);
+
+	return 0;
+}
