96ff8614d2503c5a78724796f4ef829061a3384d
hyohyeon01 <162984147+hyohyeon01@users.noreply.github.com>
2024-03-30 20:11:36 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 368 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int a;
+	int b; 
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d %d", &a, &b);
+		printf("%d\n", a + b);
+	}
+
+
+	return 0;
+}
