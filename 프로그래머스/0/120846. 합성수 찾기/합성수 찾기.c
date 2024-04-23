f2e0d6308a4e6dbea53baa300f4650df2e2ae458
hyohyeon01 <162984147+hyohyeon01@users.noreply.github.com>
2024-04-24 08:36:14 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    
+    for(int i = 1; i <=n; i++){
+        int cnt = 0;
+        for(int j = 1; j <= n; j++){
+            if(i % j == 0){
+                cnt++;
+            }
+        }
+        if(cnt >=3){
+            answer++;
+        }
+    }
+    return answer;
+}
