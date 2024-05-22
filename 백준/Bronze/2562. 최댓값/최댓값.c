9dafba7732d14bbaab4c202f7dbfae66f7ce09c0
hyohyeon01 <162984147+hyohyeon01@users.noreply.github.com>
2024-05-22 16:50:34 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.c
+++ 백준/Bronze/2562. 최댓값/최댓값.c
@@ -1,20 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void) {
-	int a[100];
-	int max = 0;
-	int index = 0;
 
-	for (int i = 0; i < 9; i++)
-	{
-		scanf("%d", &a[i]);
-		if (max < a[i])
-		{
-			max = a[i];
-			index = i + 1;
+	int num[10];
+	int max = 0;
+	int a = 0;
+	for (int i = 0; i < 9; i++) {
+		scanf("%d", &num[i]);
+		if (num[i] > max) {
+			max = num[i];
+			a = i + 1;
 		}
 	}
-	printf("%d\n%d", max, index);
+	printf("%d\n", max);
+	printf("%d", a);
 
 	return 0;
 }
